766199391d3ed4a0ec266d7ee2f06b3c12e6c97d
Ben Norquist <norquist.ben@gmail.com>
2018-08-24 22:37:47 -0700

    only username and password in .env

--- login.py
+++ login.py
@@ -11,12 +11,12 @@ def login(login_url, username, password, username_id, password_id):
     username_box.send_keys(username)
     password_box.send_keys(password)
 
-    login_button = browser.find_element_by_css_selector('input.submit')
+    login_button = browser.find_element_by_css_selector(settings.BTN_LOGIN_BUTTON_CSS)
     login_button.submit()
 
 
 if __name__ == '__main__':
-    login(login_url='https://broadcasthe.net/login.php',
+    login(login_url=settings.BTN_LOGIN_URL,
           username=settings.BTN_USERNAME,
           password=settings.BTN_PASSWORD,
           password_id=settings.BTN_PASSWORD_ID,
--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ import os
 
 load_dotenv(find_dotenv())
 
+# BTN
+BTN_LOGIN_URL = 'https://broadcasthe.net/login.php'
 BTN_USERNAME = os.environ('BTN_USERNAME')
 BTN_PASSWORD = os.environ('BTN_PASSWORD')
-BTN_USERNAME_ID = os.environ('BTN_USERNAME_ID')
-BTN_PASSWORD_ID = os.environ('BTN_PASSWORD_ID')
+BTN_USERNAME_ID = 'username'
+BTN_PASSWORD_ID = 'password'
+BTN_LOGIN_BUTTON_CSS = 'input.submit'
